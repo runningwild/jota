e1cb18ee7a9519dc723d03c85b15f9a55c733f8c
Jonathan Wills <runningwild@gmail.com>
2013-07-27 22:54:43 -0700

    Good progress on some smooth ui stuffs

--- gui/button.go
+++ gui/button.go
@@ -2,41 +2,48 @@ package gui
 
 import (
 	"fmt"
-	gl "github.com/chsc/gogl/gl21"
 	"github.com/runningwild/glop/gin"
 	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/magnus/base"
 )
 
 type Button struct {
-	Name  string
-	Count int
-	Hover bool
-	Last  Region
+	Name         string
+	Count        int
+	Hover        bool
+	Last         Region
+	Size         int
+	renderString string
+	Callback     func()
 }
 
 func (b *Button) Think(gui *Gui) {
 	x, y := gui.sys.GetCursorPos()
 	b.Hover = x >= b.Last.X && x < b.Last.X+b.Last.Dx &&
 		y >= b.Last.Y && y < b.Last.Y+b.Last.Dy
+	b.renderString = fmt.Sprintf("%d: %s", b.Count, b.Name)
 }
 func (b *Button) Respond(eventGroup gin.EventGroup) {
 	if !b.Hover {
 		return
 	}
 	if found, event := eventGroup.FindEvent(gin.AnyMouseLButton); found && event.Type == gin.Press {
-		b.Count++
+		if b.Callback != nil {
+			b.Callback()
+		} else {
+			b.Count++
+		}
 	}
 }
 func (b *Button) Draw(region Region) {
 	b.Last = region
 	if b.Hover {
-		gl.Color4ub(255, 255, 255, 255)
+		gui.SetFontColor(1, 1, 0, 1)
 	} else {
-		gl.Color4ub(255, 255, 255, 128)
+		gui.SetFontColor(1, 1, 0, 0.5)
 	}
-	base.GetDictionary("luxisr").RenderString(fmt.Sprintf("%d: %s", b.Count, b.Name), float64(region.X), float64(region.Y), 0, 30, gui.Left)
+	base.GetDictionary("luxisr").RenderString(b.renderString, float64(region.X), float64(region.Y), 0, float64(b.Size), gui.Left)
 }
 func (b *Button) RequestedDims() Dims {
-	return Dims{int(base.GetDictionary("luxisr").StringWidth(b.Name, 30)), 30}
+	return Dims{int(base.GetDictionary("luxisr").StringWidth(b.renderString, float64(b.Size))), b.Size}
 }
---
+++ gui/thunder_menu.go
@@ -0,0 +1,138 @@
+package gui
+
+import (
+	gl "github.com/chsc/gogl/gl21"
+	"github.com/runningwild/glop/gin"
+)
+
+type ThunderSubMenu struct {
+	Options  []Widget
+	requests map[Widget]Dims
+}
+
+func (tsm *ThunderSubMenu) Draw(region Region) {
+	gl.Disable(gl.TEXTURE_2D)
+	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+	gl.Enable(gl.BLEND)
+	gl.Begin(gl.QUADS)
+	x := gl.Int(region.X)
+	y := gl.Int(region.Y)
+	dx := gl.Int(region.Dx)
+	dy := gl.Int(region.Dy)
+	gl.Vertex2i(x, y)
+	gl.Vertex2i(x, y+dy)
+	gl.Vertex2i(x+dx, y+dy)
+	gl.Vertex2i(x+dx, y)
+	gl.End()
+	for _, option := range tsm.Options {
+		region.Dy = tsm.requests[option].Dy
+		option.Draw(region)
+		region.Y += tsm.requests[option].Dy
+	}
+}
+
+type ThunderMenu struct {
+	Subs      map[string]*ThunderSubMenu
+	menuStack []string
+	inTransit float64
+	request   Dims
+}
+
+func (tm *ThunderMenu) Think(gui *Gui) {
+	if tm.inTransit != 0 {
+		tm.inTransit *= 0.6
+		threshold := 0.001
+		if tm.inTransit < threshold && tm.inTransit > -threshold {
+			if tm.inTransit < 0 {
+				tm.menuStack = tm.menuStack[0 : len(tm.menuStack)-1]
+			}
+			tm.inTransit = 0
+		}
+		return
+	}
+	tsm := tm.Subs[tm.menuStack[len(tm.menuStack)-1]]
+	for key := range tsm.Options {
+		tsm.Options[key].Think(gui)
+	}
+}
+
+func (tm *ThunderMenu) Push(target string) {
+	if tm.inTransit != 0 {
+		return
+	}
+	tm.inTransit = 1.0
+	tm.menuStack = append(tm.menuStack, target)
+}
+
+func (tm *ThunderMenu) Pop() {
+	if tm.inTransit != 0 {
+		return
+	}
+	tm.inTransit = -1.0
+}
+
+func (tm *ThunderMenu) Respond(eventGroup gin.EventGroup) {
+	if tm.inTransit != 0 {
+		return
+	}
+	tsm := tm.Subs[tm.menuStack[len(tm.menuStack)-1]]
+	for _, option := range tsm.Options {
+		option.Respond(eventGroup)
+	}
+}
+
+func (tm *ThunderMenu) Draw(region Region) {
+	if tm.inTransit != 0 {
+		var shift float64
+		if tm.inTransit < 0 {
+			shift = -tm.inTransit
+		} else {
+			shift = 1.0 - tm.inTransit
+		}
+		prevRegion := region
+		prevRegion.X -= int(float64(prevRegion.Dx) * shift)
+		gl.Color4ub(255, 0, 0, 100)
+		tm.Subs[tm.menuStack[len(tm.menuStack)-2]].Draw(prevRegion)
+	}
+	var shift float64
+	if tm.inTransit < 0 {
+		shift = 1.0 + tm.inTransit
+	} else {
+		shift = tm.inTransit
+	}
+	region.X += int(float64(region.Dx) * shift)
+	gl.Color4ub(0, 255, 0, 100)
+	tm.Subs[tm.menuStack[len(tm.menuStack)-1]].Draw(region)
+}
+
+func (tsm *ThunderSubMenu) RequestedDims() Dims {
+	if tsm.requests == nil {
+		tsm.requests = make(map[Widget]Dims)
+	}
+	var dims Dims
+	for _, option := range tsm.Options {
+		opDims := option.RequestedDims()
+		tsm.requests[option] = opDims
+		if opDims.Dx > dims.Dx {
+			dims.Dx = opDims.Dx
+		}
+		dims.Dy += opDims.Dy
+	}
+	return dims
+}
+
+func (tm *ThunderMenu) RequestedDims() Dims {
+	tm.request.Dy = 0
+	for _, sub := range tm.Subs {
+		subDims := sub.RequestedDims()
+		if subDims.Dy > tm.request.Dy {
+			tm.request.Dy = subDims.Dy
+		}
+	}
+	return tm.request
+}
+
+func (tm *ThunderMenu) Start(dx int) {
+	tm.menuStack = []string{""}
+	tm.request = Dims{Dx: dx}
+}
