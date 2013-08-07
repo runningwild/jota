a20723f9c25c1db62841554aec6eab58e4835493
Jonathan Wills <runningwild@gmail.com>
2013-08-06 19:31:40 -0700

    Door -> Portal

--- game/level.go
+++ game/level.go
@@ -5,19 +5,19 @@ import (
 	"github.com/runningwild/linear"
 )
 
-type Door struct {
+type Portal struct {
 	Region linear.Poly
 	Dest   int
 }
 
 type Room struct {
-	Walls  map[string]linear.Poly
-	Lava   map[string]linear.Poly
-	Start  linear.Vec2
-	End    linear.Vec2
-	Doors  map[string]Door
-	Dx, Dy int
-	NextId int
+	Walls   map[string]linear.Poly
+	Lava    map[string]linear.Poly
+	Start   linear.Vec2
+	End     linear.Vec2
+	Portals map[string]Portal
+	Dx, Dy  int
+	NextId  int
 }
 
 func (r *Room) AddWall(wall linear.Poly) {
