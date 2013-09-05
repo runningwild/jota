f775161439caf795f49f5cb99d3328db4d803b85
Jonathan Wills <runningwild@gmail.com>
2013-09-04 19:13:36 -0700

    Big optimization to the los cache

--- game/loscache.go
+++ game/loscache.go
@@ -2,7 +2,6 @@ package game
 
 import (
 	"github.com/runningwild/linear"
-	"github.com/runningwild/magnus/base"
 	"github.com/runningwild/magnus/los"
 	"github.com/runningwild/magnus/stats"
 	"math"
@@ -12,17 +11,17 @@ import (
 type losCache struct {
 	losBuffer *los.Los
 	wallCache *wallCache
-	cache     map[losCacheViewerPos]visiblePosSlice
+	cache     map[losCacheViewerPos][]visiblePos
 	dx, dy    int
 }
 
 func makeLosCache(dx, dy int) *losCache {
 	var lc losCache
 	lc.losBuffer = los.Make(stats.LosPlayerHorizon)
-	lc.cache = make(map[losCacheViewerPos]visiblePosSlice)
+	lc.cache = make(map[losCacheViewerPos][]visiblePos)
 	lc.wallCache = &wallCache{}
-	lc.dx = dx
-	lc.dy = dy
+	lc.dx = dx / LosGridSize
+	lc.dy = dy / LosGridSize
 	return &lc
 }
 
@@ -36,107 +35,47 @@ type visiblePos struct {
 	Dist float64
 }
 
-type visiblePosSlice []visiblePos
+type visiblePosInternal struct {
+	X, Y  int
+	Dist  float64
+	Angle float64
+	Index int
+}
 
-func (vps visiblePosSlice) Len() int           { return len(vps) }
-func (vps visiblePosSlice) Less(i, j int) bool { return vps[i].Dist < vps[j].Dist }
-func (vps visiblePosSlice) Swap(i, j int)      { vps[i], vps[j] = vps[j], vps[i] }
+type visiblePosInternalSlice []visiblePosInternal
+
+func (vps visiblePosInternalSlice) Len() int           { return len(vps) }
+func (vps visiblePosInternalSlice) Less(i, j int) bool { return vps[i].Dist < vps[j].Dist }
+func (vps visiblePosInternalSlice) Swap(i, j int)      { vps[i], vps[j] = vps[j], vps[i] }
 
 func (lc *losCache) SetWallCache(wc *wallCache) {
 	lc.wallCache = wc
-	lc.cache = make(map[losCacheViewerPos]visiblePosSlice)
+	lc.cache = make(map[losCacheViewerPos][]visiblePos)
 }
 
-// Includes all possible visiblePos values in order of distance
-var maxVps visiblePosSlice
+// Includes all possible visiblePosInternal values in order of distance
+var maxVps visiblePosInternalSlice
 
+// Set up maxVps
 func init() {
 	max := stats.LosPlayerHorizon / LosGridSize
 	for x := -max; x <= max; x++ {
 		for y := -max; y <= max; y++ {
-			maxVps = append(maxVps, visiblePos{
-				X:    x,
-				Y:    y,
-				Dist: math.Sqrt(float64(x*x + y*y)),
+			dist := math.Sqrt(float64(x*x + y*y))
+			if dist > float64(max) {
+				continue
+			}
+			angle := math.Atan2(float64(y), float64(x))
+			maxVps = append(maxVps, visiblePosInternal{
+				X:     x,
+				Y:     y,
+				Dist:  dist,
+				Angle: angle,
+				Index: int(((angle/(2*math.Pi))+0.5)*float64(los.Resolution)) % los.Resolution,
 			})
 		}
 	}
 	sort.Sort(maxVps)
-}
-
-func (lc *losCache) Get2(i, j int, maxDist float64) []visiblePos {
-	vp := losCacheViewerPos{i, j}
-	if vps, ok := lc.cache[vp]; ok {
-		var max int
-		for max = 0; max < len(vps) && vps[max].Dist <= maxDist; max++ {
-		}
-		return vps[0:max]
-	}
-	pos := linear.Vec2{float64(i), float64(j)}
-	lc.losBuffer.Reset(pos)
-	var vps visiblePosSlice
-	for _, wall := range lc.wallCache.GetWalls(i, j) {
-		mid := wall.P.Add(wall.Q).Scale(0.5)
-		if mid.Sub(pos).Mag() < stats.LosPlayerHorizon+wall.Ray().Mag() {
-			lc.losBuffer.DrawSeg(wall, "")
-		}
-	}
-	dx0 := (int(pos.X+0.5) - stats.LosPlayerHorizon) / LosGridSize
-	dx1 := (int(pos.X+0.5) + stats.LosPlayerHorizon) / LosGridSize
-	dy0 := (int(pos.Y+0.5) - stats.LosPlayerHorizon) / LosGridSize
-	dy1 := (int(pos.Y+0.5) + stats.LosPlayerHorizon) / LosGridSize
-	for x := dx0; x <= dx1; x++ {
-		if x < 0 || x >= lc.dx {
-			continue
-		}
-		for y := dy0; y <= dy1; y++ {
-			if y < 0 || y >= lc.dy {
-				continue
-			}
-			seg := linear.Seg2{
-				pos,
-				linear.Vec2{(float64(x) + 0.5) * LosGridSize, (float64(y) + 0.5) * LosGridSize},
-			}
-			dist2 := seg.Ray().Mag2()
-			if dist2 > stats.LosPlayerHorizon*stats.LosPlayerHorizon {
-				continue
-			}
-			raw := lc.losBuffer.RawAccess()
-			angle := math.Atan2(seg.Ray().Y, seg.Ray().X)
-			index := int(((angle/(2*math.Pi))+0.5)*float64(len(raw))) % len(raw)
-			if dist2 < stats.LosPlayerHorizon*stats.LosPlayerHorizon {
-				val := 255.0
-				if dist2 < float64(raw[index]) {
-					val = 0
-				} else if dist2 < float64(raw[(index+1)%len(raw)]) ||
-					dist2 < float64(raw[(index+len(raw)-1)%len(raw)]) {
-					val = 100
-				} else if dist2 < float64(raw[(index+2)%len(raw)]) ||
-					dist2 < float64(raw[(index+len(raw)-2)%len(raw)]) {
-					val = 200
-				}
-				fade := 100.0
-				if dist2 > (stats.LosPlayerHorizon-fade)*(stats.LosPlayerHorizon-fade) {
-					val = 255 - (255-val)*(1.0-(fade-(stats.LosPlayerHorizon-math.Sqrt(dist2)))/fade)
-				}
-				if val < 255 {
-					vps = append(vps, visiblePos{
-						X:    x,
-						Y:    y,
-						Val:  byte(val),
-						Dist: math.Sqrt(dist2),
-					})
-				}
-			}
-		}
-	}
-	sort.Sort(vps)
-	lc.cache[vp] = vps
-	base.Log().Printf("%d,%d: %2.2v", i, j, vps[0:3])
-	// Call this function again because now the values are cached.  We do this
-	// because we also need to slice the return value appropriately for maxDist
-	// and shouldn't have that logic in two places.
-	return lc.Get(i, j, maxDist)
 }
 
 func (lc *losCache) Get(i, j int, maxDist float64) []visiblePos {
@@ -149,46 +88,41 @@ func (lc *losCache) Get(i, j int, maxDist float64) []visiblePos {
 	}
 	pos := linear.Vec2{float64(i), float64(j)}
 	lc.losBuffer.Reset(pos)
-	// TODO: how to allocate at the beginning?
-	vps := make(visiblePosSlice, stats.LosPlayerHorizon/LosGridSize)[0:0]
+	vps := make([]visiblePos, len(maxVps))[0:0]
 	for _, wall := range lc.wallCache.GetWalls(i, j) {
 		mid := wall.P.Add(wall.Q).Scale(0.5)
 		if mid.Sub(pos).Mag() < stats.LosPlayerHorizon+wall.Ray().Mag() {
 			lc.losBuffer.DrawSeg(wall, "")
 		}
 	}
+	ix := int(pos.X / LosGridSize)
+	iy := int(pos.Y / LosGridSize)
 	for _, v := range maxVps {
-		x := int(pos.X/LosGridSize+0.5) + v.X
-		y := int(pos.Y/LosGridSize+0.5) + v.Y
+		x := ix + v.X
+		y := iy + v.Y
 		if x < 0 || x >= lc.dx {
 			continue
 		}
 		if y < 0 || y >= lc.dy {
 			continue
 		}
-		seg := linear.Seg2{
-			linear.Vec2{0, 0},
-			linear.Vec2{(float64(v.X)) * LosGridSize, (float64(v.Y)) * LosGridSize},
-		}
 		raw := lc.losBuffer.RawAccess()
-		angle := math.Atan2(seg.Ray().Y, seg.Ray().X)
-		index := int(((angle/(2*math.Pi))+0.5)*float64(len(raw))) % len(raw)
 		val := 255.0
 		dist := v.Dist * LosGridSize
 		distSq := dist * dist
-		if distSq < float64(raw[index]) {
+		if distSq < float64(raw[v.Index]) {
 			val = 0
-			// } else if distSq < float64(raw[(index+1)%len(raw)]) ||
-			// 	distSq < float64(raw[(index+len(raw)-1)%len(raw)]) {
-			// 	val = 100
-			// } else if distSq < float64(raw[(index+2)%len(raw)]) ||
-			// 	distSq < float64(raw[(index+len(raw)-2)%len(raw)]) {
-			// 	val = 200
+		} else if distSq < float64(raw[(v.Index+1)%len(raw)]) ||
+			distSq < float64(raw[(v.Index+len(raw)-1)%len(raw)]) {
+			val = 100
+		} else if distSq < float64(raw[(v.Index+2)%len(raw)]) ||
+			distSq < float64(raw[(v.Index+len(raw)-2)%len(raw)]) {
+			val = 200
 		}
-		// fade := 100.0
-		// if dist > stats.LosPlayerHorizon-fade {
-		// 	val = 255 - (255-val)*(1.0-(fade-(stats.LosPlayerHorizon-dist))/fade)
-		// }
+		fade := 100.0
+		if dist > stats.LosPlayerHorizon-fade {
+			val = 255 - (255-val)*(1.0-(fade-(stats.LosPlayerHorizon-dist))/fade)
+		}
 		if val < 255 {
 			vps = append(vps, visiblePos{
 				X:    x,
